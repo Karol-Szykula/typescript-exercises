fa470e45530a41d606ecf859b7230589a6defaa9
Karol szykula <karol.szykula@gmail.com>
2018-12-12 13:35:12 +0100

    Add SearchViewPropsInterface interface

---
+++ Counter.ts
@@ -0,0 +1,4 @@
+interface Counter {
+    value: number
+}
+
---
+++ SearchViewPropsInterface.ts
@@ -0,0 +1,9 @@
+interface SearchViewPropsInterface {
+    onSearchTextChangeHandler: Function
+    maxSearchedNumberOfQuestionsInTest: number
+    onSearchSliderValueChangeHandler: Function
+    searchedNumberOfQuestionsInTest: number
+    chosenCategoryFilter: number
+    onSearchSelectFieldValueChangeHandler: Function
+    categoryFilters: Array<String>
+}
